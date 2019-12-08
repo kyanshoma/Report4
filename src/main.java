7de6ccff4d62017de80ada4718ec55cd1235d1f1
Shoma KYAN <e175773@ie.u-ryukyu.ac.jp>
2019-12-08 13:04:58 +0900

    ステップ3

--- src/main.java
+++ src/main.java
@@ -1,11 +1,6 @@
 public class main {
     public static void main(String[] args){
-        try {
-            String str = null;
-            str.length();
-        }catch (NullPointerException e){
-            System.out.println(e + "が発生");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
